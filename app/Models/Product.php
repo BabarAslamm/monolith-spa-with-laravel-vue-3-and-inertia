6a6e89563245e252cb69b119c707cca75b8a08de
BabarAslamm <babaraslam825@gmail.com>
2025-04-08 22:44:12 +0500

    Relation functions added in models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
